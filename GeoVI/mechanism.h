46cace317b6a13a822885ca9d394a5c7ab0febd7
wuwei <2295920467@qq.com>
2023-11-05 12:40:08 +0800

    add some enum

--- GeoVI/mechanism.h
+++ GeoVI/mechanism.h
@@ -1,25 +1,60 @@
 #ifndef MECHANISM_H
 #define MECHANISM_H
 
+#include <map>
+
 namespace geovi {
     namespace algorithm{
         namespace mechanism {
             enum Attack {
-                Optimal
+                Optimal,
+                Beyans
             };
 
             enum DistanceMeasurement {
                 Euclid
             };
 
+            enum ScoreType {
+                AE,
+                Q_loss,
+                PC,
+                Time
+            };
+
             class Mechanism {
             public:
                 using Attack = enum Attack;
                 using DistanceMeasurement = enum DistanceMeasurement;
-                void buildDistribution(float epsilon);
-                void computerInferenceFunction();
-                void computerAE(Attack attack);
+                using ScoreType = enum ScoreType;
+                using Score = std::map<ScoreType,double>;
+
+                virtual void buildDistribution(float _epsilon);
+                virtual void computerInferenceFunction();
+                virtual void computerAE(Attack attack);
+                virtual void computerPC();
+                virtual void computerQ_loss();
+                Score score();
+
+            protected:
+                float epsilon;
+                float start_time;
+                float end_time;
             private:
+                
+            };
+
+
+            class PLMG : public Mechanism {
+
+            };
+
+            class GEM : public Mechanism {
+
+            };
+
+            class GVEM : public Mechanism {
+
             };
         }
     }
